58041e446001623ed2bcca3be6e0fc62046ce032
Your Name <luismateusjuniorsibia@gmail.com>
2024-07-30 00:44:51 -0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 p-4 text-white">
+    <header className="bg-blue-500 p-6 text-white">
       <nav className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">MyApp</h1>
-        <ul className="flex space-x-4">
+        <h1 className="text-xl font-bold">IT Isibia</h1>
+        <ul className="flex space-x-8">
           <li><Link to="/" className="hover:underline">Home</Link></li>
           <li><Link to="/login" className="hover:underline">Login</Link></li>
           <li><Link to="/signup" className="hover:underline">Sign Up</Link></li>
